460416ecac0ea5fa01e0b40494647f177b4d7e27
testoccgg <testoccgg@gmail.com>
2023-10-25 11:57:45 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+
+  <ProductDetails v-for="item in products" :key="item.id" :nameItem="item"></ProductDetails>
+
+
+</template>
+
+<script>
+import ProductDetails from './components/ProductDetails.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ProductDetails
+  },
+  data () {
+    return {
+      products: [{
+        name: 'apple',
+        price: 200,
+        availiable: true
+      },
+      {
+        name: 'pineapple',
+        price: 300,
+        availiable: false
+      },
+      {
+        name: 'orange',
+        price: 100,
+        availiable: true
+      }
+      ]
+    }
+  }
+}
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/ProductDetails.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    {{ nameItem }}
+  </div>
+</template>
+<script>
+export default {
+  name: 'ProductDetails',
+  props: ['nameItem']
+
+}
+</script>
+
+<style lang="scss" scoped></style>
